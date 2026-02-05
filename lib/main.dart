8c1c974011d5ec710d391603072326abbf47fd7f
Shoaib <shoaibkhan1447026@gmail.com>
2026-02-05 19:49:27 +0500

    Firebase connected (web, android, ios)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+
   runApp(MyApp());
+
+
 }
 
 class MyApp extends StatelessWidget {
