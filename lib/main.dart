33571f42bfa31d83667dcd1b36b5247cb6c087b9
shoaib7026 <shoaibkhan1447026@gmail.com>
2026-02-06 10:56:57 +0500

    class me pull keya tha and firestore set keya tha

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,13 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
-
   runApp(MyApp());
-
-
 }
+
+
 
 class MyApp extends StatelessWidget {
   @override
@@ -25,3 +22,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
